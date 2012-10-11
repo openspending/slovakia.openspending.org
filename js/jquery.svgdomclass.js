b0abad058c71268bb290e42da1d3d405526fa246
Vitor Baptista <vitor@vitorbaptista.com>
2012-10-10 21:43:17 -0300

    Patch JQuery's (add|remove|toggle|has)Class methods to work with SVG DOM.
    The SVG DOM elements have a different API that the XML DOM. So, JQuery's class
    manipulation methods doesn't work. We've monkey-patched them simply by changing
    these two calls:
    
      elem.className  =>  elem.getAttribute("class")
      elem.className = "new_class"  =>  elem.setAttribute("class", "new_class")

---
+++ js/jquery.svgdomclass.js
@@ -0,0 +1,126 @@
+(function ($) {
+  var core_rspace = " ",
+      rclass = /[\t\r\n]/g;
+  
+  function addClass(value) {
+  	var classNames, i, l, elem,
+  		setClass, c, cl;
+  
+  	if ( $.isFunction( value ) ) {
+  		return this.each(function( j ) {
+  			$( this ).addClass( value.call(this, j, this.className) );
+  		});
+  	}
+  
+  	if ( value && typeof value === "string" ) {
+  		classNames = value.split( core_rspace );
+  
+  		for ( i = 0, l = this.length; i < l; i++ ) {
+  			elem = this[ i ];
+  
+  			if ( elem.nodeType === 1 ) {
+  				if ( !elem.getAttribute("class") && classNames.length === 1 ) {
+  					elem.setAttribute("class", value);
+  
+  				} else {
+  					setClass = " " + elem.getAttribute("class") + " ";
+  
+  					for ( c = 0, cl = classNames.length; c < cl; c++ ) {
+  						if ( setClass.indexOf( " " + classNames[ c ] + " " ) < 0 ) {
+  							setClass += classNames[ c ] + " ";
+  						}
+  					}
+  					elem.setAttribute("class", $.trim( setClass ));
+  				}
+  			}
+      }
+    }
+  }
+  
+  function removeClass( value ) {
+  	var removes, className, elem, c, cl, i, l;
+  
+  	if ( $.isFunction( value ) ) {
+  		return this.each(function( j ) {
+  			$( this ).removeClass( value.call(this, j, this.className) );
+  		});
+  	}
+  	if ( (value && typeof value === "string") || value === undefined ) {
+  		removes = ( value || "" ).split( core_rspace );
+  
+  		for ( i = 0, l = this.length; i < l; i++ ) {
+  			elem = this[ i ];
+  			if ( elem.nodeType === 1 && elem.getAttribute("class") ) {
+  
+  				className = (" " + elem.getAttribute("class") + " ").replace( rclass, " " );
+  
+  				// loop over each item in the removal list
+  				for ( c = 0, cl = removes.length; c < cl; c++ ) {
+  					// Remove until there is nothing to remove,
+  					while ( className.indexOf(" " + removes[ c ] + " ") >= 0 ) {
+  						className = className.replace( " " + removes[ c ] + " " , " " );
+  					}
+  				}
+  				elem.setAttribute("class", value ? $.trim( className ) : "");
+  			}
+  		}
+  	}
+  
+  	return this;
+  }
+  
+  function toggleClass( value, stateVal ) {
+  	var type = typeof value,
+  		isBool = typeof stateVal === "boolean";
+  
+  	if ( $.isFunction( value ) ) {
+  		return this.each(function( i ) {
+  			$( this ).toggleClass( value.call(this, i, this.className, stateVal), stateVal );
+  		});
+  	}
+  
+  	return this.each(function() {
+  		if ( type === "string" ) {
+  			// toggle individual class names
+  			var className,
+  				i = 0,
+  				self = $( this ),
+  				state = stateVal,
+  				classNames = value.split( core_rspace );
+  
+  			while ( (className = classNames[ i++ ]) ) {
+  				// check each className given, space separated list
+  				state = isBool ? state : !self.hasClass( className );
+  				self[ state ? "addClass" : "removeClass" ]( className );
+  			}
+  
+  		} else if ( type === "undefined" || type === "boolean" ) {
+  			if ( this.className ) {
+  				// store className if set
+  				$._data( this, "__className__", this.className );
+  			}
+  
+  			// toggle whole className
+  			this.setAttribute("class", this.getAttribute("class") || value === false ? "" : $._data( this, "__className__" ) || "");
+  		}
+  	});
+  }
+  
+  function hasClass( selector ) {
+  	var className = " " + selector + " ",
+  		i = 0,
+  		l = this.length;
+  	for ( ; i < l; i++ ) {
+  		if ( this[i].nodeType === 1 && (" " + this[i].getAttribute("class") + " ").replace(rclass, " ").indexOf( className ) >= 0 ) {
+  			return true;
+  		}
+  	}
+  
+  	return false;
+  }
+
+  $.fn.removeClass = removeClass;
+  $.fn.addClass = addClass;
+  $.fn.toggleClass = toggleClass;
+  $.fn.hasClass = hasClass;
+})(jQuery);
