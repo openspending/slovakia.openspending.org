3a62abf88f8c1f2c085f9549362a2c2e5bac9628
Vitor Baptista <vitor@vitorbaptista.com>
2013-01-07 23:11:46 -0300

    Remove Total and Tooltip changes and update OpenSpendingJS.
    I've moved these changes to upstream, so we don't need them in our codebase
    anymore.

--- js/treetable.js
+++ js/treetable.js
@@ -1,17 +1,11 @@
 OpenSpending.Treetable = function (context, drilldowns) {
-
   function render(dataset, state, callback) {
     var render_ctx = _.extend(context, {'dataset': dataset});
     var treemap_ctx = _.extend(render_ctx, {
-      click: function(node) { callback(node.data.name); },
-      tooltipMessage: function(widget, node) {
-        var percentualValue = (node.data.value * 100)/widget.total;
-        return node.name + ' (' + (percentualValue).toFixed(2) + '%)';
-      }
+      click: function(node) { callback(node.data.name); }
     });
 
     new OpenSpending.AggregateTable($('#table_widget'), render_ctx, state).then(function(widget) {
-      widget.calculateRowsValues = _.wrap(widget.calculateRowsValues, _addTotalRowToResults)
       widget.$e.unbind('click', 'td a');
       widget.$e.on('click', 'td a', function(e) {
         var name = $(e.target).data('name') + '';
@@ -39,30 +33,6 @@ OpenSpending.Treetable = function (context, drilldowns) {
         callback(name, filters, currentDrilldown);
       }
     });
-  }
-
-  function _addTotalRowToResults(calculateRowsValues, data) {
-    var rows = calculateRowsValues(data);
-    var total = _calculateTotalRow(data, rows);
-
-    rows.push(total);
-
-    return rows;
-  }
-
-  function _calculateTotalRow(data) {
-    var total = {
-      __amount_pct: 1,
-      amount: data.summary.amount
-    };
-
-    _.each(drilldowns, function (drilldown) {
-      total[drilldown] = {
-        label: 'Total'
-      };
-    });
-
-    return total;
   }
 
   return {
